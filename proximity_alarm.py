aa6fe96e2003f6b75c17d744ab1eba7d513ba9d7
Ronald Sims II <ron@orangemantis.net>
2019-02-03 21:53:13 -0500

    Added setup.

--- proximity_alarm.py
+++ proximity_alarm.py
@@ -1,16 +1,19 @@
-import PRi.GPIO as gpio
+import RPi.GPIO as gpio
 import time
 
-gpio.cleanup()
 gpio.setmode(gpio.BOARD)
 
 proxim_pin = 3
 
+gpio.setup(proxim_pin, gpio.IN, gpio.PUD_DOWN)
+
 loop_limit = 999999999
 loop_count = 0
 
-while loop_count > loop_limit:
+while loop_count < loop_limit:
     if gpio.input(proxim_pin):
         print('Nearby object found')
     loop_count +=1
     time.sleep(0.33)
+
+gpio.cleanup()
