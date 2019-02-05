3e5c88357ce2cb72b1a06e627e05fa72f9a0d4d3
Ronald Sims II <ron@orangemantis.net>
2019-02-04 21:27:04 -0500

    Looking to light led when motion detected...pin not outputing.

--- proximity_alarm.py
+++ proximity_alarm.py
@@ -3,16 +3,20 @@ import time
 
 gpio.setmode(gpio.BOARD)
 
-proxim_pin = 3
+proxim_pin = 11
+led_pin = 13
 
 gpio.setup(proxim_pin, gpio.IN, gpio.PUD_DOWN)
+gpio.setup(led_pin, gpio.OUT)
 
 loop_limit = 999999999
 loop_count = 0
 
 while loop_count < loop_limit:
+    gpio.output(led_pin, False)
     if gpio.input(proxim_pin):
         print('Nearby object found')
+        gpio.output(led_pin, True)
     loop_count +=1
     time.sleep(0.33)
 
